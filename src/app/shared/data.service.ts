4a00760fa4e1ce6e7ff77e89e3f607f947889962
Jeremy <jp5486@gmail.com>
2016-04-18 05:41:07 -0700

    add tokens to data service http requests for user auth

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -8,28 +8,28 @@ export class DataService{
   constructor (private _http: Http) {}
 
   getAllData(): Observable<any> {
-    // const token = localStorage.getItem('token') !== null ? '?auth=' +localStorage.getItem('token') : '';
-    // return this._http.get('https://sleepdiary.firebaseio.com/users/data.json' + token)
-    return this._http.get('https://sleepdiary.firebaseio.com/users/data.json')
+    const token = localStorage.getItem('token') !== null ? '?auth=' +localStorage.getItem('token') : '';
+    return this._http.get('https://sleepdiary.firebaseio.com/users/data.json' + token)
+    // return this._http.get('https://sleepdiary.firebaseio.com/users/data.json')
       .map(response => response.json());
   }
 
   addData(data: any): Observable<any> {
-    // const token = localStorage.getItem('token') !== null ? '?auth=' + localStorage.getItem('token') : '';
+    const token = localStorage.getItem('token') !== null ? '?auth=' + localStorage.getItem('token') : '';
     const body = JSON.stringify(data);
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
 
-    // return this._http.post('https://sleepdiary.firebaseio.com/users/data.json' + token, body, {headers: headers})
-    return this._http.post('https://sleepdiary.firebaseio.com/users/data.json', body, {headers: headers})
+    return this._http.post('https://sleepdiary.firebaseio.com/users/data.json' + token, body, {headers: headers})
+    // return this._http.post('https://sleepdiary.firebaseio.com/users/data.json', body, {headers: headers})
       .map(response => response.json());
   }
 
   // need to modify per specific post
   deleteAllData(): Observable<any> {
-    // const token = localStorage.getItem('token') !== null ? '?auth=' + localStorage.getItem('token') : '';
-    // return this._http.delete('https://sleepdiary.firebaseio.com/users/data.json'+ token)
-    return this._http.delete('https://sleepdiary.firebaseio.com/users/data.json')
+    const token = localStorage.getItem('token') !== null ? '?auth=' + localStorage.getItem('token') : '';
+    return this._http.delete('https://sleepdiary.firebaseio.com/users/data.json'+ token)
+    // return this._http.delete('https://sleepdiary.firebaseio.com/users/data.json')
       .map(response => response.json());
   }
 }
