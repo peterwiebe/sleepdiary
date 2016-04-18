d24e64e0c79110b4d10153e08c8a24cfc6dce84e
Jeremy <jp5486@gmail.com>
2016-04-18 03:35:23 -0700

    Make Auth Service available globally in app

--- src/app.ts
+++ src/app.ts
@@ -2,5 +2,6 @@ import {bootstrap} from 'angular2/platform/browser';
 import {SleepdiaryApp} from './app/sleepdiary';
 import {ROUTER_PROVIDERS} from "angular2/router";
 import {HTTP_PROVIDERS} from "angular2/http";
+import {AuthService} from "./app/shared/auth.service";
 
-bootstrap(SleepdiaryApp, [ROUTER_PROVIDERS, HTTP_PROVIDERS]);
+bootstrap(SleepdiaryApp, [ROUTER_PROVIDERS, HTTP_PROVIDERS, AuthService]);
